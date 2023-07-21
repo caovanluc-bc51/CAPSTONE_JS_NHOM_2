9c3ad7a2d0cdeed4a26051ad0dca5fcdda454c83
ttrangtrdg28 <ttrangtrdg28@gmail.com>
2023-07-21 23:14:59 +0700

    update shop-online search, cart

--- shop-online/js/shop.js
+++ shop-online/js/shop.js
@@ -34,7 +34,7 @@ function renderUI(data) {
               <h3 class="cardPhone__title">${product.name}</h3>
               <h3 class="cardPhone__price">$${product.price}</h3>
               <div>
-                <button class="btnPhone-shadow" type="button" data-toggle="modal" data-target="#chiTietSP" onclick = productDetail('${product.id}')>
+                <button class="btnPhone-shadow" type="button" data-toggle="modal" data-target="#productDetailPopup" onclick = productDetail('${product.id}')>
                   <i class="fa-solid fa-shopping-cart"></i> Add to cart
                 </button>
               </div>
@@ -64,4 +64,55 @@ function productDetail(idSP) {
     .catch(function (error) {
       console.log(error);
     });
+}
+
+function searchProductByType() {
+  var search = getEle("category").value;
+  // console.log(search);
+
+  var promise = api.getListProductApi();
+  promise
+    .then(function (result) {
+      var productList = [];
+      for (var i = 0; i < result.data.length; i++) {
+        if (result.data[i].type === search) {
+          productList.push(result.data[i]);
+          renderUI(productList);
+        } else if (search === "All") {
+          productList.push(result.data[i]);
+          renderUI(productList);
+        }
+      }
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+}
+
+function renderProductCartUI(data) {
+  var content = "";
+  for (var i = 0; i < data.length; i++) {
+    var product = data[i];
+    content += `
+        <div class="col-12 col-md-6 col-lg-4 my-2">
+          <div class="card cardPhone">
+            <img
+              src="${product.img}"
+              class="card-img-top"
+              alt="..."
+            />
+            <div class="card-body">
+              <h3 class="cardPhone__title">${product.name}</h3>
+              <h3 class="cardPhone__price">$${product.price}</h3>
+              <div>
+                <button class="btnPhone-shadow" type="button" data-toggle="modal" data-target="#productDetailPopup" onclick = productDetail('${product.id}')>
+                  <i class="fa-solid fa-shopping-cart"></i> Add to cart
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+        `;
+  }
+  getEle("productList").innerHTML = content;
 }
